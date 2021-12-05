4560307750b2fb3803c29903e3670cf2d09a188c
Daniil <danil.dobrodeev@mail.ru>
2021-12-05 22:48:47 +0300

    translate to russian

--- src/Main.kt
+++ src/Main.kt
@@ -2,9 +2,9 @@ fun main() {
     val data = listOf(10f, 100f, 1000f)
     for (i in 0 until 9)
     {
-        print("number ${i + 1}\tsize ${data[i % 3]}\tinterval [-${data[i % 3]}; ${data[i % 3]}]\t")
+        print("Номер теста ${i + 1}\tРазмерность ${data[i % 3]}\tДиапазон значений [-${data[i % 3]}; ${data[i % 3]}]\t")
         Tester().test(20, data[i / 3], data[i % 3].toInt()).also {
-            println("eps avg ${it.first}\tavg mark ${it.second}")
+            println("Средняя относительная погрешность ${it.first}\tСреднее значение оценки точности ${it.second}")
         }
     }
 }
